31ddf47052bf4e79660e43ea4a32ee667ce406d1
Anton <door3218@gmail.com>
2018-12-05 17:50:51 +0400

    первый комит

---
+++ lesson_18/script.js
@@ -0,0 +1,49 @@
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var main = document.getElementById("main");
+var myList = document.getElementById("list");
+var ul = document.getElementsByTagName("ul");
+var story = myList.getElementsByClassName("item");
+var popup = document.getElementsByClassName("popup")[0];
+var close = document.querySelector(".close");
+
+function addItem(){
+	var newLi = document.createElement("li");
+	newLi.innerHTML = "Новая задача";
+	newLi.className = "item";
+	myList.appendChild(newLi);
+	popup.style.display = "none";
+}
+function delItem(){
+	myList.removeChild(story[0]);
+
+	if(story.length == 0){
+		popup.style.display = "block";
+	}
+}
+function closePopup(){
+	popup.style.display = "none";
+}
+
+btn1.addEventListener("click", addItem);
+btn2.addEventListener("click", delItem);
+close.addEventListener("click", closePopup);
+
+
+
+var age = 21;
+var user = "meow";
+var subs = true;
+
+function welcome(){
+	console.log("welcome");
+}
+
+if(age > 17) {
+	if(subs == true){
+		console.log(welcome);
+	}
+}
+else {
+	console.log("no access");
+}
